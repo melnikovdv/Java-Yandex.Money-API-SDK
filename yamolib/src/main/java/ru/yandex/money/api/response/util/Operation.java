fdb61b6dd8c17b4395b25504c4a31a2ab76e5ef0
OneHalf_3544 <onehalf354@gmail.com>
2013-10-26 00:33:44 +0400

    Перенес поля в объект Operation (полные данные можно запрашивать и из operation-history)

--- yamolib/src/main/java/ru/yandex/money/api/response/util/Operation.java
+++ yamolib/src/main/java/ru/yandex/money/api/response/util/Operation.java
@@ -21,6 +21,11 @@ public class Operation implements Serializable {
     protected BigDecimal amount;
     protected Date datetime;
     protected String title;
+    protected String sender;
+    protected String recipient;
+    protected String message;
+    protected Boolean codepro;
+    protected String details;
 
     protected Operation() {
     }
@@ -67,11 +72,66 @@ public class Operation implements Serializable {
     }
 
     /**
-     * @return Краткое описание операции (название
-     * магазина или источник пополнения).
+     * @return Краткое описание операции (название магазина или источник пополнения).
      */
     public String getTitle() {
         return title;
+    }
+
+    /**
+     * Поле присутствует только при запросе operation-history с параметром details=true,
+     * или при запросе operation-detail
+     *
+     * @return возвращает номер счета отправителя перевода. Присутствует для
+     * входящих переводов от других пользователей.
+     */
+    public String getSender() {
+        return sender;
+    }
+
+    /**
+     * Поле присутствует только при запросе operation-history с параметром details=true,
+     * или при запросе operation-detail
+     *
+     * @return возвращает номер счета отправителя перевода. Присутствует для
+     * входящих переводов от других пользователей.
+     */
+    public String getRecipient() {
+        return recipient;
+    }
+
+    /**
+     * Поле присутствует только при запросе operation-history с параметром details=true,
+     * или при запросе operation-detail
+     *
+     * @return возвращает комментарий к переводу. Присутствует для
+     * переводов другим пользователям.
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * Поле присутствует только при запросе operation-history с параметром details=true,
+     * или при запросе operation-detail
+     *
+     * @return возвращает признак перевод защищен кодом протекции.
+     * Присутствует для переводов другим пользователям.
+     */
+    public Boolean getCodepro() {
+        return codepro;
+    }
+
+    /**
+     * Поле присутствует только при запросе operation-history с параметром details=true,
+     * или при запросе operation-detail
+     *
+     * @return возвращает детальное описание платежа.
+     * Строка произвольного формата, может содержать любые символы и
+     * переводы строк.
+     */
+    public String getDetails() {
+        return details;
     }
 
     @Override
@@ -79,10 +139,15 @@ public class Operation implements Serializable {
         return "Operation{" +
                 "operationId='" + operationId + '\'' +
                 ", patternId='" + patternId + '\'' +
-                ", direction='" + direction + '\'' +
+                ", title='" + title + '\'' +
+                ", direction=" + direction +
                 ", amount=" + amount +
                 ", datetime=" + datetime +
-                ", title='" + title + '\'' +
+                ", sender='" + sender + '\'' +
+                ", recipient='" + recipient + '\'' +
+                ", message='" + message + '\'' +
+                ", codepro=" + codepro +
+                ", details='" + details + '\'' +
                 '}';
     }
 }
