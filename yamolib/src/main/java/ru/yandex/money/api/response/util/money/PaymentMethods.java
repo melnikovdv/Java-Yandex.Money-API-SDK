3650cca06ee3550cd7ea92c374606e05e252c35d
sergeev <sergeev@yamoney.ru>
2013-11-18 19:50:22 +0400

    Данные сквозной карты

--- yamolib/src/main/java/ru/yandex/money/api/response/util/money/PaymentMethods.java
+++ yamolib/src/main/java/ru/yandex/money/api/response/util/money/PaymentMethods.java
@@ -10,7 +10,7 @@ package ru.yandex.money.api.response.util.money;
 public class PaymentMethods {
 
     private Element wallet = new Element();
-    private Element card = new Element();
+    private CardElement card = new CardElement();
 
     private PaymentMethods() {
     }
@@ -45,7 +45,7 @@ public class PaymentMethods {
      */
     public class Element {
 
-        private boolean allowed = false;
+        protected boolean allowed = false;
 
         private Element() {
         }
@@ -63,6 +63,33 @@ public class PaymentMethods {
         public String toString() {
             return "Element{" +
                     "allow=" + allowed +
+                    '}';
+        }
+    }
+
+    public class CardElement extends Element {
+        /**
+         * Признак разрешенности платежа с карты без card security code
+         * Возможен для некоторых типов платежей для которых низка вероятность мошенничества.
+         */
+        private boolean csc_required = true;
+        /**
+         * Маскированный номер карты
+         */
+        private String pan_fragment;
+
+        /**
+         * Тип карты (VISA, MasterCard)
+         */
+        private String type;
+
+        @Override
+        public String toString() {
+            return "CardElement{" +
+                    "allow=" + allowed +
+                    ", csc_required=" + csc_required +
+                    ", pan_fragment='" + pan_fragment + '\'' +
+                    ", type='" + type + '\'' +
                     '}';
         }
     }
