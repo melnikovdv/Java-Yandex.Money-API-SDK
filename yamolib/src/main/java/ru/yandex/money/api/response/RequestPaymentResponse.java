30d697412594bfce50c737aef4d7d338b2e764b2
sergeev <sergeev@yamoney.ru>
2014-03-13 13:26:46 +0400

    Добавлен запрос доп.параметров контракта

--- yamolib/src/main/java/ru/yandex/money/api/response/RequestPaymentResponse.java
+++ yamolib/src/main/java/ru/yandex/money/api/response/RequestPaymentResponse.java
@@ -7,6 +7,7 @@ import ru.yandex.money.api.response.util.money.PaymentMethods;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.Map;
 
 /**
  * <p>Класс для возврата результата метода requestPayment</p>
@@ -29,6 +30,7 @@ public class RequestPaymentResponse implements Serializable {
     private Boolean recipient_identified;
     private String recipient_account_type;
     private Boolean test_payment;
+    private Map<String, String> contract_details;
 
     private RequestPaymentResponse() {
     }
@@ -149,6 +151,15 @@ public class RequestPaymentResponse implements Serializable {
         return recipient_identified;
     }
 
+    /**
+     * Доп. параметры контракта.
+     * Присутствуют при плаетже в магазин, если в запросе присутствовал параметр show_contract_details=true
+     * @return
+     */
+    public Map<String, String> getContractDetails() {
+        return contract_details;
+    }
+
     @Override
     public String toString() {
         return "RequestPaymentResponse{" +
@@ -158,6 +169,7 @@ public class RequestPaymentResponse implements Serializable {
                 ", moneySource=" + moneySource +
                 ", requestId='" + requestId + '\'' +
                 ", contract='" + contract + '\'' +
+                ", contract_details='" + contract_details + '\'' +
                 ", balance=" + balance +
                 ", test_payment=" + test_payment +
                 ", recipient_identified=" + recipient_identified +
